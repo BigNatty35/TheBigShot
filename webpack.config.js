2cdd7d6a6ff5708bfa8f1b82d975e2227e8d0368
Carey Johnson <johnson.careyj@gmail.com>
2018-06-18 01:16:50 -0700

    Got my webpack up and running, installed the dependencies and entry file.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./lib/cannons",
+  output: {
+    filename: "./lib/bundle"
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
